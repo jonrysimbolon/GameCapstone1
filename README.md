a88a0f50688ade21e08546e81eaf077e8f819420
jonrysimbolon <jonrysimbolon19@gmail.com>
2024-07-01 14:25:40 +0700

    adding changes on readme, config.yml and build.gradle.kts(:project)

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Game Capstone 1 android expert
+[![jonrysimbolon](https://circleci.com/gh/jonrysimbolon/GameCapstone1.svg?style=svg)](https://circleci.com/gh/jonrysimbolon/GameCapstone1)
 
 - API GAME [https://api.rawg.io/docs/]
 - KOIN
 - FLOW
 - CLEAN ARCHITECTURE
-- PRESENTATION, DOMAIN, DATA
 - MODULARIZATION (CORE, FAVORITE - DYNAMIC FEATURE)
 - DIFFERENT BETWEEN DOMAIN'S MODEL AND DATA'S MODEL
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,14 @@ plugins {
     alias(libs.plugins.android.dynamic.feature) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.ksp) apply false
+    id("org.owasp.dependencycheck") version "9.2.0" apply false
+}
+
+allprojects {
+    apply(plugin = "org.owasp.dependencycheck")
+}
+
+configure<org.owasp.dependencycheck.gradle.extension.DependencyCheckExtension> {
+    format = org.owasp.dependencycheck.reporting.ReportGenerator.Format.ALL.toString()
+    nvd.apiKey = "f123f392-e61a-4714-aff9-13248a66583d"
 }
