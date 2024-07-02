10ffa2301370694f3deb06f5b29df90146f3bf2a
jonrysimbolon <jonrysimbolon19@gmail.com>
2024-07-02 19:19:16 +0700

    other

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.CategoryFilter.exclude
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
