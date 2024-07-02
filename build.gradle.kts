45b69aac564bc1dbd1c2c647d9b3fe3a8e2b7473
jonrysimbolon <jonrysimbolon19@gmail.com>
2024-07-02 15:36:55 +0700

    upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,53 +8,15 @@ plugins {
     alias(libs.plugins.ksp) apply false
 }
 
-/*allprojects {
-    tasks.register("printAllDependenciesHtml") {
-        doLast {
-            val htmlFile = File(buildDir, "reports/dependencies.html")
-            htmlFile.parentFile.mkdirs()
-            htmlFile.writeText("<html><body><pre>")
-            subprojects.forEach { subproject ->
-                subproject.configurations.forEach { configuration ->
-                    htmlFile.appendText("${subproject.name} - ${configuration.name}\n")
-                    configuration.allDependencies.forEach { dependency ->
-                        htmlFile.appendText("${dependency.group}:${dependency.name}:${dependency.version}\n")
-                    }
-                }
-            }
-            htmlFile.appendText("</pre></body></html>")
-        }
-    }
-}*/
-
 allprojects {
-    tasks.register("printAllDependenciesHtml") {
-        doLast {
-            val htmlFile = File(buildDir, "reports/dependencies.html")
-            htmlFile.parentFile.mkdirs()
-            htmlFile.writeText("<html><body><pre>")
+    tasks.register("printAllDependencies", DependencyReportTask::class)
+}
 
-            // Function to recursively print dependencies
-            fun printDependencies(project: Project, depth: Int) {
-                val indentation = "\t".repeat(depth)
-                project.configurations.forEach { configuration ->
-                    htmlFile.appendText("${indentation}${project.name} - ${configuration.name}\n")
-                    configuration.allDependencies.forEach { dependency ->
-                        htmlFile.appendText("${indentation}${dependency.group}:${dependency.name}:${dependency.version}\n")
-                        if (dependency.group == null) {
-                            project.rootProject.findProject(dependency.name)
-                                ?.let { printDependencies(it, depth + 1) }
-                        }
-                    }
-                }
-            }
-
-            // Call printDependencies for each subproject
-            subprojects.forEach { subproject ->
-                printDependencies(subproject, 0)
-            }
-
-            htmlFile.appendText("</pre></body></html>")
-        }
+configurations {
+    all {
+        exclude(group = "com.google.guava", module = "listenablefuture")
+        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib")
+        exclude(group = "io.netty", module = "netty-handler")
+        exclude(group = "io.netty", module = "netty-codec-http2")
     }
 }
